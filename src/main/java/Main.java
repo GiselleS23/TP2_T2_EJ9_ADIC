68c8dda1b07c0c8a789aaa825132ff01e3e1bd0f
GiselleS23 <128999381+GiselleS23@users.noreply.github.com>
2024-05-09 16:07:32 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,41 @@
+/* 9.- Construya un algoritmo tal que, dados los valores enteros P y Q, determine si los mismos satisfacen la siguiente expresión: P3+Q+-2p2<680 En caso afirmativo debe imprimir los valores PyQ. 
+Sacchetti, Maria Giselle C2 */
+/* EJEMPLO DE VALORES QUE SATISFACEN: P=6 Y Q=2 
+p=0 q=0; */
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    /* int p, q; */
+    int resultado;
+
+    System.out.println("Ingresar el valor de P:");
+    int p = scanner.nextInt();
+
+    System.out.println("Ingresar el valor de Q:");
+    int q = scanner.nextInt();
+    //uso math.pow para elevar a las potencias 3 y 2
+    resultado = (int) Math.pow(p, 3) + (int) Math.pow(q, 2);
+    if (resultado < 680) {
+      System.out.println("Los valores de P y Q satisfacen la expresión");
+    } else {
+      System.out.println("Los valores de P y Q no satisfacen la expresión");
+    }
+    scanner.close();
+
+  }
+
+}
+/*
+  int p = 2;
+  int q = 3;
+  int resultado = 0;
+
+ resultado = (int) (Math.pow(p, 3)) + q - 2 * (int) (Math.pow(p, 2));
+
+ if(resultado < 680){
+ System.out.println("P: " + p + " Q: " + q); }
+ */
